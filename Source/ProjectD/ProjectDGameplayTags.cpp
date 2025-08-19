0a337c74ab82ec204b2862bf2303faf3afacf3cb
DESKTOP-OAPN4UI\Gunn <gunn0467@gmail.com>
2025-08-19 20:55:42 +0900

    [ProjectD] ProjectDGameplayTags 추가

---
+++ Source/ProjectD/ProjectDGameplayTags.cpp
@@ -0,0 +1,8 @@
+
+#include "ProjectDGameplayTags.h"
+
+namespace ProjectDGameplayTags
+{
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Move, "InputTag.Move");
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Look, "InputTag.Look");
+}
---
+++ Source/ProjectD/ProjectDGameplayTags.h
@@ -0,0 +1,10 @@
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace ProjectDGameplayTags
+{
+	PROJECTD_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Move);
+	PROJECTD_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Look);
+}
