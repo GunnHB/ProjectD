e5c8f90058118e2ea26542d2fa3b580f14266b25
DESKTOP-OAPN4UI\Gunn <gunn0467@gmail.com>
2025-08-20 17:20:18 +0900

    [ProjectD] GameplayAbility 추가

---
+++ Source/ProjectD/AbilitySystem/Abilities/ProjectDGameplayAbility.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ProjectDGameplayAbility.h"
+
+#include "AbilitySystemComponent.h"
+
+void UProjectDGameplayAbility::OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec)
+{
+	Super::OnGiveAbility(ActorInfo, Spec);
+
+	if (AbilityActivationPolicy == EProjectDAbilityActivationPolicy::OnGiven)
+	{
+		if (ActorInfo && Spec.IsActive() == false)
+			ActorInfo->AbilitySystemComponent->TryActivateAbility(Spec.Handle);
+	}
+}
+
+void UProjectDGameplayAbility::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+{
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+
+	if (AbilityActivationPolicy == EProjectDAbilityActivationPolicy::OnGiven)
+	{
+		if (ActorInfo)
+			ActorInfo->AbilitySystemComponent->ClearAbility(Handle);
+	}
+}
---
+++ Source/ProjectD/AbilitySystem/Abilities/ProjectDGameplayAbility.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "ProjectDGameplayAbility.generated.h"
+
+UENUM()
+enum class EProjectDAbilityActivationPolicy
+{
+	OnTriggered,
+	OnGiven,
+};
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTD_API UProjectDGameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+protected:
+	// ~ Begin IAbilitySystemInterface Interface
+	virtual void OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec) override;
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+	// ~ End IAbilitySystemInterface Interface
+
+	UPROPERTY(EditDefaultsOnly, Category="CharacterAbility")
+	EProjectDAbilityActivationPolicy AbilityActivationPolicy = EProjectDAbilityActivationPolicy::OnTriggered;
+};
