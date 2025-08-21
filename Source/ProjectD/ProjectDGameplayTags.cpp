66363024c1b0e2e9c1d1e8487aaf21c3a152079a
DESKTOP-OAPN4UI\Gunn <gunn0467@gmail.com>
2025-08-21 12:47:10 +0900

    [ProjectD] 게임플레이 태그 추가

--- Source/ProjectD/ProjectDGameplayTags.cpp
+++ Source/ProjectD/ProjectDGameplayTags.cpp
@@ -3,6 +3,13 @@
 
 namespace ProjectDGameplayTags
 {
+	/* Input Tags */
 	UE_DEFINE_GAMEPLAY_TAG(InputTag_Move, "InputTag.Move");
 	UE_DEFINE_GAMEPLAY_TAG(InputTag_Look, "InputTag.Look");
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_LightAttack, "InputTag.LightAttack");
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_HeavyAttack, "InputTag.HeavyAttack");
+
+	/* Player Tags */
+	UE_DEFINE_GAMEPLAY_TAG(Player_Ability_Attack_Light, "Player.Ability.Attack.Light");
+	UE_DEFINE_GAMEPLAY_TAG(Player_Ability_Attack_Heavy, "Player.Ability.Attack.Heavy");
 }
