2325a0cac40c23722f706f4c178556879a1b34ca
DESKTOP-OAPN4UI\Gunn <gunn0467@gmail.com>
2025-08-25 18:18:07 +0900

    [ProjectD] 이벤트 태그 추가

--- Source/ProjectD/ProjectDGameplayTags.cpp
+++ Source/ProjectD/ProjectDGameplayTags.cpp
@@ -6,8 +6,8 @@ namespace ProjectDGameplayTags
 	/* Input Tags */
 	UE_DEFINE_GAMEPLAY_TAG(InputTag_Move, "InputTag.Move");
 	UE_DEFINE_GAMEPLAY_TAG(InputTag_Look, "InputTag.Look");
-	UE_DEFINE_GAMEPLAY_TAG(InputTag_LightAttack, "InputTag.LightAttack");
-	UE_DEFINE_GAMEPLAY_TAG(InputTag_HeavyAttack, "InputTag.HeavyAttack");
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Attack_Light, "InputTag.Attack.Light");
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Attack_Heavy, "InputTag.Attack.Heavy");
 
 	/* Player Tags */
 	UE_DEFINE_GAMEPLAY_TAG(Player_Ability_Attack_Light, "Player.Ability.Attack.Light");
@@ -16,6 +16,6 @@ namespace ProjectDGameplayTags
 	/* Shared Tags */
 	UE_DEFINE_GAMEPLAY_TAG(Shared_Status_Combat, "Shared.Status.Combat");
 	
-	UE_DEFINE_GAMEPLAY_TAG(Shared_Event_EnterCombat, "Shared.Event.EnterCombat");
-	UE_DEFINE_GAMEPLAY_TAG(Shared_Event_LeaveCombat, "Shared.Event.LeaveCombat");
+	UE_DEFINE_GAMEPLAY_TAG(Shared_Event_Combat, "Shared.Event.Combat");
+	UE_DEFINE_GAMEPLAY_TAG(Shared_Event_Combat_ActionTaken, "Shared.Event.Combat.ActionTaken");
 }
