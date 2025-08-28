ff22380d28b898170223d04363536f15ca90fac0
DESKTOP-OAPN4UI\Gunn <gunn0467@gmail.com>
2025-08-28 18:17:21 +0900

    [ProjectD] DataAsset_CombatConfig 추가

---
+++ Source/ProjectD/DataAssets/Combat/DataAsset_CombatConfig.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "DataAsset_CombatConfig.generated.h"
+
+class UComboNode;
+/**
+ * 
+ */
+UCLASS(BlueprintType)
+class PROJECTD_API UDataAsset_CombatConfig : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<UComboNode> RootNode = nullptr;
+};
