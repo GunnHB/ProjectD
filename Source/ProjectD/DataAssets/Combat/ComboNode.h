8dd29282846c91d2900389ec6429f30e3438963e
DESKTOP-OAPN4UI\Gunn <gunn0467@gmail.com>
2025-08-28 18:16:59 +0900

    [ProjectD] ComboNode 추가

---
+++ Source/ProjectD/DataAssets/Combat/ComboNode.h
@@ -0,0 +1,19 @@
+﻿
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "ComboNode.generated.h"
+
+UCLASS(BlueprintType)
+class UComboNode : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Combat")
+	TObjectPtr<UAnimMontage> AnimMontage = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Combat", meta=(DisplayName="Next Combo Nodes"), Instanced)
+	TMap<FGameplayTag, UComboNode*> NextComboNodes;
+};
