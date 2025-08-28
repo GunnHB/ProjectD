1c442b39ba7ad617f42944b9d0b34d960024ef73
DESKTOP-OAPN4UI\Gunn <gunn0467@gmail.com>
2025-08-28 13:34:12 +0900

    [ProjectD] HeavyAttack 태그 삭제

--- Source/ProjectD/ProjectDGameplayTags.cpp
+++ Source/ProjectD/ProjectDGameplayTags.cpp
@@ -7,11 +7,9 @@ namespace ProjectDGameplayTags
 	UE_DEFINE_GAMEPLAY_TAG(InputTag_Move, "InputTag.Move");
 	UE_DEFINE_GAMEPLAY_TAG(InputTag_Look, "InputTag.Look");
 	UE_DEFINE_GAMEPLAY_TAG(InputTag_Attack_Light, "InputTag.Attack.Light");
-	UE_DEFINE_GAMEPLAY_TAG(InputTag_Attack_Heavy, "InputTag.Attack.Heavy");
 
 	/* Player Tags */
 	UE_DEFINE_GAMEPLAY_TAG(Player_Ability_Attack_Light, "Player.Ability.Attack.Light");
-	UE_DEFINE_GAMEPLAY_TAG(Player_Ability_Attack_Heavy, "Player.Ability.Attack.Heavy");
 
 	/* Shared Tags */
 	UE_DEFINE_GAMEPLAY_TAG(Shared_Status_Combat, "Shared.Status.Combat");
